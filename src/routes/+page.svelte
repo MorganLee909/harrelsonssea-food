578aefdba55dc18de7a84a0b41f88eb3845040a5
Lee Morgan <lee@leemorgan.dev>
2024-10-10 10:43:18 -0400

    Create the header.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import "../global.css";
+    import Header from "./Header.svelte";
+
+    import oysterAndKnife from "$lib/oysterAndKnife.webp";
+    import localPeople from "$lib/localPeople.webp";
+</script>
+
+<div class="container">
+    <Header/>
+
+    <div class="sectionTwo">
+        <div class="partOne">
+            <img src={oysterAndKnife} alt="Oyster knife sitting next to opened oysters"/>
+            <h1>Local Seafood</h1>
+        </div>
+
+        <div class="partTwo">
+            <img src={localPeople} alt="Two people smiling behind cooking ingredients"/>
+            <h1>...from Good Local People</h1>
+        </div>
+    </div>
+</div>
+
+<style>
+    .sectionTwo{
+        background: rgb(0, 0, 25);
+        color: white;
+    }
+
+    .sectionTwo h1{
+        color: white;
+        font-size: 48px;
+    }
+
+    .sectionTwo img{
+        width: 35%;
+    }
+
+    .partOne{
+        margin: 35px;
+    }
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,66 @@
+<script>
+    import storeFront from "$lib/harrelsons1.webp";
+</script>
+
+<header>
+    <img src={storeFront} alt="Harrelsons Storefront">
+    <div class="imageCover"></div>
+
+    <div class="menu">
+        <h1>Harrelsons Seafood</h1>
+        <h2>Fresh Fish Daily</h2>
+        <h3>Directly from Local Fishermen</h3>
+    </div>
+</header>
+
+<style>
+    header{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh; 
+        width: 100%;
+        position: relative;
+    }
+
+    img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: -2;
+    }
+
+    .imageCover{
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: -1;
+        background: rgba(0, 0, 0, 0.5);
+    }
+
+    .menu{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+        height: 100%;
+        color: white;
+    }
+
+    .menu h1{
+        font-size: 68px;
+    }
+
+    .menu h2{
+        font-size: 48px;
+    }
+
+    .menu h3{
+        font-size: 38px;
+    }
+</style>
